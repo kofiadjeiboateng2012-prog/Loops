27d51350701fb9c91c330ad3aef448b93126343f
kofiadjeiboateng2012-prog <kofiadjeiboateng2012@gmail.com>
2026-08-24 19:33:05 -0400

    Add files via upload

---
+++ Loops Activity2.py
@@ -0,0 +1,7 @@
+string=input("Enter a  word which you would like to see  it's reversed form")
+string2=("")
+for i in string:
+    string2=i+string2
+print("The original word",string)
+print("The reversed word",string2)
+
